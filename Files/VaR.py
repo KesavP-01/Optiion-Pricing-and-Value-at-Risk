a04fc2fc32431670d77e2b0feb04d5c83bdda1bf
Kesav Ponduru <161378031+KesavP-01@users.noreply.github.com>
2024-06-22 15:07:29 -0500

    Add files via upload

---
+++ Files/VaR.py
@@ -0,0 +1,23 @@
+import numpy as np
+import yfinance as yf
+from scipy.stats import norm
+
+
+def download_data(stock, start_date, end_date):
+    df = yf.download(stock, start_date, end_date)
+    df = df[['Adj Close']]
+    df['returns'] = df['Adj Close'].pct_change()
+    return df
+
+# n - VaR after n days
+def calculate_var_n(stock_data, position, c, n):
+    df = stock_data
+    mu = np.mean(df['returns'])
+    sigma = np.std(df['returns'])
+    var = position * (mu * n - sigma * np.sqrt(n) * norm.ppf(1-c))
+    return var
+
+
+stock = download_data('META', '2022-04-02', '2024-03-01')
+
+Var = calculate_var_n(stock, 100000, 0.99, 1)
